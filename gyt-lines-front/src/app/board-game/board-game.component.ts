d221d7f23dbe0935962e9c27531b1da17d7a9b92
Schneider-Maunoury Timothee <timothee.schneider-maunoury@etudiant.univ-rennes1.fr>
2019-05-06 20:25:53 +0200

    Creation of the board component

---
+++ gyt-lines-front/src/app/board-game/board-game.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-board-game',
+  templateUrl: './board-game.component.html',
+  styleUrls: ['./board-game.component.css']
+})
+export class BoardGameComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  gameState = 'en cours ...'
+
+}
