7e73d6b98cc460996e99b7e5fbb95404c759acc2
Yoann <yoann4997@hotmail.fr>
2019-05-22 15:06:16 +0200

    mock plate

--- gyt-lines-front/src/app/board-game/board-game.component.ts
+++ gyt-lines-front/src/app/board-game/board-game.component.ts
@@ -9,8 +9,20 @@ import { GameService } from '../game.service';
 
 export class BoardGameComponent implements OnInit {
 
-  game : GameService;
-  size : number [] = Array.from(Array(8), (x, index) => index + 1);
+  game: GameService;
+  size: number [] = Array.from(Array(8), (x, index) => index);
+
+
+  plate = [
+    [0, 1, 1, 1, 1, 1, 1, 0],
+    [1, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 1],
+    [0, 1, 1, 1, 1, 1, 1, 0],
+  ]
 
   constructor(gameService: GameService) { 
     this.game = gameService;
@@ -24,9 +36,25 @@ export class BoardGameComponent implements OnInit {
     this.game.initGame();
   }
 
+  getPawnId(row: number, col: number) {
+    return row+col;
+  }
+
+  hasPawn(row: number, col: number) {
+    return this.plate[row][col];
+  }
+
+  getColor(row: number, col: number) {
+      return (col === 0 || col === (this.size.length - 1)) ? {background: 'white'} : {background: 'black'};
+  }
+
+  onClickPawn(event) {
+    console.log(event.target.id);
+  }
+
   // hasPawn(row: Number, col: Number){
   //   for(let pawn of pawns){
-  //     if(pawn.x == col && pawn.y = row){
+  //     if(pawn.x == col && pawn.y = row) {
   //       return true;
   //     }
   //     return false;
