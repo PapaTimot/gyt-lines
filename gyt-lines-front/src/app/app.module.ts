f3aa498667d19f208c0cb771d3de263159e2ebf9
JOUNEAUX Gwendal <jouneaux.gwendal@laposte.net>
2019-05-22 11:03:40 +0200

    Revert "Creation of the internal structure of the game"

--- gyt-lines-front/src/app/app.module.ts
+++ gyt-lines-front/src/app/app.module.ts
@@ -2,12 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { BoardGameComponent } from './board-game/board-game.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    BoardGameComponent
+    AppComponent
   ],
   imports: [
     BrowserModule
