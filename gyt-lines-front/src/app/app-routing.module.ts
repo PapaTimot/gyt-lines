6cb06215e690c8e0cf2679dfeeaf475d13e96bc0
Yoann <yoann4997@hotmail.fr>
2019-05-23 11:50:08 +0200

    Credit + board game turn

--- gyt-lines-front/src/app/app-routing.module.ts
+++ gyt-lines-front/src/app/app-routing.module.ts
@@ -2,11 +2,15 @@ import { NgModule }             from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MenuComponent }        from './menu/menu.component';
 import { BoardGameComponent }      from './board-game/board-game.component';
+import { InstructionsComponent } from './instructions/instructions.component'
+import { CreditsComponent } from './credits/credits.component'
 
 const routes: Routes = [
   { path: ''    , redirectTo: '/menu', pathMatch: 'full' },
   { path: 'menu', component: MenuComponent      },
-  { path: 'game', component: BoardGameComponent }
+  { path: 'game', component: BoardGameComponent },
+  { path: 'instructions', component: InstructionsComponent },
+  { path: 'credits', component: CreditsComponent }
 ];
 
 @NgModule({
